45d78214439eaca4228a29f9c80e57a11aa3b7e2
Peter Zhukovsky <38474927+peterzhukovsky@users.noreply.github.com>
2021-03-17 09:34:44 -0400

    Add files via upload

---
+++ network_PAL_YrsD.m
@@ -0,0 +1,268 @@
+llabel_names={'L_10d_ROI.label';'L_10pp_ROI.label';'L_10r_ROI.label';'L_10v_ROI.label';'L_11l_ROI.label';'L_13l_ROI.label';'L_1_ROI.label';'L_23c_ROI.label';'L_23d_ROI.label';'L_24dd_ROI.label';'L_24dv_ROI.label';'L_25_ROI.label';'L_2_ROI.label';'L_31a_ROI.label';'L_31pd_ROI.label';'L_31pv_ROI.label';'L_33pr_ROI.label';'L_3a_ROI.label';'L_3b_ROI.label';'L_43_ROI.label';'L_44_ROI.label';'L_45_ROI.label';'L_46_ROI.label';'L_47l_ROI.label';'L_47m_ROI.label';'L_47s_ROI.label';'L_4_ROI.label';'L_52_ROI.label';'L_55b_ROI.label';'L_5L_ROI.label';'L_5m_ROI.label';'L_5mv_ROI.label';'L_6a_ROI.label';'L_6d_ROI.label';'L_6ma_ROI.label';'L_6mp_ROI.label';'L_6r_ROI.label';'L_6v_ROI.label';'L_7AL_ROI.label';'L_7Am_ROI.label';'L_7m_ROI.label';'L_7PC_ROI.label';'L_7PL_ROI.label';'L_7Pm_ROI.label';'L_8Ad_ROI.label';'L_8Av_ROI.label';'L_8BL_ROI.label';'L_8BM_ROI.label';'L_8C_ROI.label';'L_9-46d_ROI.label';'L_9a_ROI.label';'L_9m_ROI.label';'L_9p_ROI.label';'L_a10p_ROI.label';'L_A1_ROI.label';'L_a24pr_ROI.label';'L_a24_ROI.label';'L_a32pr_ROI.label';'L_a47r_ROI.label';'L_A4_ROI.label';'L_A5_ROI.label';'L_a9-46v_ROI.label';'L_AAIC_ROI.label';'L_AIP_ROI.label';'L_AVI_ROI.label';'L_d23ab_ROI.label';'L_d32_ROI.label';'L_DVT_ROI.label';'L_EC_ROI.label';'L_FEF_ROI.label';'L_FFC_ROI.label';'L_FOP1_ROI.label';'L_FOP2_ROI.label';'L_FOP3_ROI.label';'L_FOP4_ROI.label';'L_FOP5_ROI.label';'L_FST_ROI.label';'L_H_ROI.label';'L_i6-8_ROI.label';'L_IFJa_ROI.label';'L_IFJp_ROI.label';'L_IFSa_ROI.label';'L_IFSp_ROI.label';'L_Ig_ROI.label';'L_IP0_ROI.label';'L_IP1_ROI.label';'L_IP2_ROI.label';'L_IPS1_ROI.label';'L_LBelt_ROI.label';'L_LIPd_ROI.label';'L_LIPv_ROI.label';'L_LO1_ROI.label';'L_LO2_ROI.label';'L_LO3_ROI.label';'L_MBelt_ROI.label';'L_MIP_ROI.label';'L_MI_ROI.label';'L_MST_ROI.label';'L_MT_ROI.label';'L_OFC_ROI.label';'L_OP1_ROI.label';'L_OP2-3_ROI.label';'L_OP4_ROI.label';'L_p10p_ROI.label';'L_p24pr_ROI.label';'L_p24_ROI.label';'L_p32pr_ROI.label';'L_p32_ROI.label';'L_p47r_ROI.label';'L_p9-46v_ROI.label';'L_PBelt_ROI.label';'L_PCV_ROI.label';'L_PeEc_ROI.label';'L_PEF_ROI.label';'L_PFcm_ROI.label';'L_PFm_ROI.label';'L_PFop_ROI.label';'L_PF_ROI.label';'L_PFt_ROI.label';'L_PGi_ROI.label';'L_PGp_ROI.label';'L_PGs_ROI.label';'L_PHA1_ROI.label';'L_PHA2_ROI.label';'L_PHA3_ROI.label';'L_PH_ROI.label';'L_PHT_ROI.label';'L_PI_ROI.label';'L_Pir_ROI.label';'L_PIT_ROI.label';'L_pOFC_ROI.label';'L_PoI1_ROI.label';'L_PoI2_ROI.label';'L_POS1_ROI.label';'L_POS2_ROI.label';'L_PreS_ROI.label';'L_ProS_ROI.label';'L_PSL_ROI.label';'L_RI_ROI.label';'L_RSC_ROI.label';'L_s32_ROI.label';'L_s6-8_ROI.label';'L_SCEF_ROI.label';'L_SFL_ROI.label';'L_STGa_ROI.label';'L_STSda_ROI.label';'L_STSdp_ROI.label';'L_STSva_ROI.label';'L_STSvp_ROI.label';'L_STV_ROI.label';'L_TA2_ROI.label';'L_TE1a_ROI.label';'L_TE1m_ROI.label';'L_TE1p_ROI.label';'L_TE2a_ROI.label';'L_TE2p_ROI.label';'L_TF_ROI.label';'L_TGd_ROI.label';'L_TGv_ROI.label';'L_TPOJ1_ROI.label';'L_TPOJ2_ROI.label';'L_TPOJ3_ROI.label';'L_V1_ROI.label';'L_v23ab_ROI.label';'L_V2_ROI.label';'L_V3A_ROI.label';'L_V3B_ROI.label';'L_V3CD_ROI.label';'L_V3_ROI.label';'L_V4_ROI.label';'L_V4t_ROI.label';'L_V6A_ROI.label';'L_V6_ROI.label';'L_V7_ROI.label';'L_V8_ROI.label';'L_VIP_ROI.label';'L_VMV1_ROI.label';'L_VMV2_ROI.label';'L_VMV3_ROI.label';'L_VVC_ROI.label'};
+
+rlabel_names={'R_10d_ROI.label';'R_10pp_ROI.label';'R_10r_ROI.label';'R_10v_ROI.label';'R_11l_ROI.label';'R_13l_ROI.label';'R_1_ROI.label';'R_23c_ROI.label';'R_23d_ROI.label';'R_24dd_ROI.label';'R_24dv_ROI.label';'R_25_ROI.label';'R_2_ROI.label';'R_31a_ROI.label';'R_31pd_ROI.label';'R_31pv_ROI.label';'R_33pr_ROI.label';'R_3a_ROI.label';'R_3b_ROI.label';'R_43_ROI.label';'R_44_ROI.label';'R_45_ROI.label';'R_46_ROI.label';'R_47l_ROI.label';'R_47m_ROI.label';'R_47s_ROI.label';'R_4_ROI.label';'R_52_ROI.label';'R_55b_ROI.label';'R_5L_ROI.label';'R_5m_ROI.label';'R_5mv_ROI.label';'R_6a_ROI.label';'R_6d_ROI.label';'R_6ma_ROI.label';'R_6mp_ROI.label';'R_6r_ROI.label';'R_6v_ROI.label';'R_7AL_ROI.label';'R_7Am_ROI.label';'R_7m_ROI.label';'R_7PC_ROI.label';'R_7PL_ROI.label';'R_7Pm_ROI.label';'R_8Ad_ROI.label';'R_8Av_ROI.label';'R_8BL_ROI.label';'R_8BM_ROI.label';'R_8C_ROI.label';'R_9-46d_ROI.label';'R_9a_ROI.label';'R_9m_ROI.label';'R_9p_ROI.label';'R_a10p_ROI.label';'R_A1_ROI.label';'R_a24pr_ROI.label';'R_a24_ROI.label';'R_a32pr_ROI.label';'R_a47r_ROI.label';'R_A4_ROI.label';'R_A5_ROI.label';'R_a9-46v_ROI.label';'R_AAIC_ROI.label';'R_AIP_ROI.label';'R_AVI_ROI.label';'R_d23ab_ROI.label';'R_d32_ROI.label';'R_DVT_ROI.label';'R_EC_ROI.label';'R_FEF_ROI.label';'R_FFC_ROI.label';'R_FOP1_ROI.label';'R_FOP2_ROI.label';'R_FOP3_ROI.label';'R_FOP4_ROI.label';'R_FOP5_ROI.label';'R_FST_ROI.label';'R_H_ROI.label';'R_i6-8_ROI.label';'R_IFJa_ROI.label';'R_IFJp_ROI.label';'R_IFSa_ROI.label';'R_IFSp_ROI.label';'R_Ig_ROI.label';'R_IP0_ROI.label';'R_IP1_ROI.label';'R_IP2_ROI.label';'R_IPS1_ROI.label';'R_LBelt_ROI.label';'R_LIPd_ROI.label';'R_LIPv_ROI.label';'R_LO1_ROI.label';'R_LO2_ROI.label';'R_LO3_ROI.label';'R_MBelt_ROI.label';'R_MIP_ROI.label';'R_MI_ROI.label';'R_MST_ROI.label';'R_MT_ROI.label';'R_OFC_ROI.label';'R_OP1_ROI.label';'R_OP2-3_ROI.label';'R_OP4_ROI.label';'R_p10p_ROI.label';'R_p24pr_ROI.label';'R_p24_ROI.label';'R_p32pr_ROI.label';'R_p32_ROI.label';'R_p47r_ROI.label';'R_p9-46v_ROI.label';'R_PBelt_ROI.label';'R_PCV_ROI.label';'R_PeEc_ROI.label';'R_PEF_ROI.label';'R_PFcm_ROI.label';'R_PFm_ROI.label';'R_PFop_ROI.label';'R_PF_ROI.label';'R_PFt_ROI.label';'R_PGi_ROI.label';'R_PGp_ROI.label';'R_PGs_ROI.label';'R_PHA1_ROI.label';'R_PHA2_ROI.label';'R_PHA3_ROI.label';'R_PH_ROI.label';'R_PHT_ROI.label';'R_PI_ROI.label';'R_Pir_ROI.label';'R_PIT_ROI.label';'R_pOFC_ROI.label';'R_PoI1_ROI.label';'R_PoI2_ROI.label';'R_POS1_ROI.label';'R_POS2_ROI.label';'R_PreS_ROI.label';'R_ProS_ROI.label';'R_PSL_ROI.label';'R_RI_ROI.label';'R_RSC_ROI.label';'R_s32_ROI.label';'R_s6-8_ROI.label';'R_SCEF_ROI.label';'R_SFL_ROI.label';'R_STGa_ROI.label';'R_STSda_ROI.label';'R_STSdp_ROI.label';'R_STSva_ROI.label';'R_STSvp_ROI.label';'R_STV_ROI.label';'R_TA2_ROI.label';'R_TE1a_ROI.label';'R_TE1m_ROI.label';'R_TE1p_ROI.label';'R_TE2a_ROI.label';'R_TE2p_ROI.label';'R_TF_ROI.label';'R_TGd_ROI.label';'R_TGv_ROI.label';'R_TPOJ1_ROI.label';'R_TPOJ2_ROI.label';'R_TPOJ3_ROI.label';'R_V1_ROI.label';'R_v23ab_ROI.label';'R_V2_ROI.label';'R_V3A_ROI.label';'R_V3B_ROI.label';'R_V3CD_ROI.label';'R_V3_ROI.label';'R_V4_ROI.label';'R_V4t_ROI.label';'R_V6A_ROI.label';'R_V6_ROI.label';'R_V7_ROI.label';'R_V8_ROI.label';'R_VIP_ROI.label';'R_VMV1_ROI.label';'R_VMV2_ROI.label';'R_VMV3_ROI.label';'R_VVC_ROI.label'};
+label_names_all=vertcat(llabel_names, rlabel_names) %combine all labels
+
+MS=zeros(1, 148); age=zeros(1,1); group=zeros(1,1); datasetNr=0; yrs_use=0; PAL=0; NART=0; BIS11=0;PAL_1stT=0;
+MS(~(age>0),:)=[];group(~(age>0))=[]; age(~(age>0))=[];id=1:length(age);
+
+X=MS_difference(yrs_use>0,:);%X=MS(group==2,:);
+Y=yrs_use(yrs_use>0); %Y=age(group==2);
+ncomp=10;
+[XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(X,Y,ncomp);
+XL_zscore(:,1)=zscore(XL(:,1));XL_zscore(:,2)=zscore(XL(:,2));XL_zscore(:,3)=zscore(XL(:,3));
+%label_names_all(XL_zscore(:,1)>1.8 | XL_zscore(:,1)<-1.8)
+label_names_all((XL_zscore(:,2)>1.5 | XL_zscore(:,2)<-1.5) & (FDR<0.01)')
+label_names_all((XL_zscore(:,1)>1.5 | XL_zscore(:,1)<-1.5) & (FDR<0.01)')
+%histogram(XL)
+%>> these are all the regions (X,Y,Z) that load highly on PLS1 and PLS2, components
+%that explain a lot of variance in the PAL; regions X, Y, Z
+%are also sign different between HC and SDI...
+%
+corr(XL_zscore(:,:),  tstat5')
+
+%% PAL
+
+X=vertcat(MS_difference_HC, MS_difference);X=MS(group<=2,:);
+X=X(PAL>-1,:);
+Y=PAL(PAL>-1); %Y=age(group==2);
+ncomp=10;
+[XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(X,Y,ncomp);
+XL_zscore(:,1)=zscore(XL(:,1));XL_zscore(:,2)=zscore(XL(:,2));XL_zscore(:,3)=zscore(XL(:,3));
+%label_names_all(XL_zscore(:,1)>1.8 | XL_zscore(:,1)<-1.8)
+label_names_all((XL_zscore(:,2)>1.8 | XL_zscore(:,2)<-1.8))
+label_names_all((XL_zscore(:,1)>1.5 | XL_zscore(:,1)<-1.5) & (FDR<0.05)')
+
+figure; scatter(XS(:,2), PAL(PAL>-1), 25,'filled'); x=XS(:,2); y=PAL(PAL>-1); corr(x,y)
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); %ylabel('GSK'); xlabel('ATX');
+
+figure; scatter(XL_zscore(:,1), tstat_roi1, 25,'filled'); x=XL_zscore(:,1); y=tstat_roi1'; corr(x,y)
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); %ylabel('GSK'); xlabel('ATX');
+corr(x,y);
+corr(XL_zscore(:,2),  tstat5_roi1')
+age1=age(group<=2); corr(XS(:,4),  age1(PAL>0))
+
+%% plotting 
+    %%%% a) PLS2 correl with yrs of use 
+figure; scatter(XS(:,2), yrs_use(yrs_use>0), 25,'filled'); x=XS(:,2); y=yrs_use(yrs_use>0); 
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); %ylabel('GSK'); xlabel('ATX');
+
+    %%%% b) pls correl with yrs of use
+figure
+subplot(2,2,1);plot(1:ncomp,cumsum(100*PCTVAR(2,:)),'-bo');
+xlabel('Number of PLS components');
+ylabel('%Variance explained in Yrs of Use');
+subplot(2,2,2); scatter(XS(:,1), Y, 10,'filled'); subplot(2,2,3); scatter(XS(:,2), Y, 10,'filled'); subplot(2,2,4); scatter(XS(:,3), Y,10,'filled'); 
+    %%%% c) one roi as an example correl with yrs of use
+corr(horzcat(XS, Y))
+%116 154 165
+%136   141   256   275   294   316
+roi=id(XL_zscore(:,2)==max(XL_zscore(:,2))); 
+roi=230;
+corr(MS_difference((yrs_use>0),roi), yrs_use(yrs_use>0))
+figure; scatter(MS_difference((yrs_use>0),roi), yrs_use(yrs_use>0), 35, 'filled')
+x=MS_difference((yrs_use>0),roi); y=yrs_use(yrs_use>0);
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); %ylabel('GSK'); xlabel('ATX');
+
+%% PLS with yrs of use, shorter, permutation check for significant differences
+clear X Y
+Y_temp=(PAL(149:331)); 
+X=zscore(MS_difference(Y_temp>-1,:));%X=MS(group==2,:);
+Y(:,1)=zscore(age_SDI(Y_temp>-1)); %Y=age(group==2);
+%Y(:,2)=zscore(Y_temp(Y_temp>-1));
+ncomp=10;
+BIS11_short=BIS11(149:331)
+BIS11_short(yrs_use>0)
+
+[XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(X,Y,ncomp);
+XL_zscore(:,1)=zscore(XL(:,1));XL_zscore(:,2)=zscore(XL(:,2));XL_zscore(:,3)=zscore(XL(:,3));
+ncomp=8;
+permutations=1000;   
+allobservations=Y; 
+
+for n = 1:permutations
+    % selecting either next combination, or random permutation
+    permutation_index = randperm(length(allobservations));
+    % creating random sample based on permutation index
+    randomSample = allobservations(permutation_index,:);
+
+    % running the PLS for this permutation
+    [XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(X,randomSample,ncomp);
+    percent_var1(n,:)=PCTVAR(1,:);
+    percent_var2(n,:)=PCTVAR(2,:);
+end
+[XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(X,Y,ncomp);
+sum(percent_var2>PCTVAR(2,:))/permutations
+
+XL
+%% PLS permutation validation with PAL and age as predictors
+clear X Y
+X=zscore(MS(group<=2,:)); 
+%X=zscore(vertcat(MS_difference_HC, MS_difference));
+X=X(PAL>-1,:);
+
+age_short=age(group<=2); %Y(:,1)=zscore(age_short);
+Y(:,2)=zscore(age_short(PAL>-1));
+Y(:,1)=zscore(PAL(PAL>-1));% & NART >0)); 
+%Y(:,3)=zscore(PAL_1stT(PAL>-1 & BIS11>1));% & NART >0)); 
+%Y(:,2)=zscore(BIS11(PAL>-1 & BIS11>1)); 
+%Y(:,3)=zscore(NART(PAL>-1 & BIS11>1 & NART >0)); 
+permutations=5000;   
+allobservations=Y; 
+
+
+for ncomp=1:8
+    
+    for n = 1:permutations
+    % selecting either next combination, or random permutation
+    permutation_index = randperm(length(allobservations));
+    % creating random sample based on permutation index
+    randomSample = allobservations(permutation_index,:);
+    % running the PLS for this permutation
+    [XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(X,randomSample,ncomp);
+    Rsq(n) = sum(PCTVAR(2,:));
+    Rsq1(n) = sum(PCTVAR(2,:));
+    end
+    [XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(X,Y,ncomp);
+    p(ncomp)=sum(sum(PCTVAR(2,:))<Rsq')/permutations
+    p_1(ncomp)=sum(sum(PCTVAR(1,:))<Rsq')/permutations
+end
+
+
+ncomp=5;
+    for n = 1:permutations
+    % selecting either next combination, or random permutation
+    permutation_index = randperm(length(allobservations));
+    % creating random sample based on permutation index
+    randomSample = allobservations(permutation_index,:);
+
+    % running the PLS for this permutation
+    [XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(X,randomSample,ncomp);
+    Rsq(n) = sum(PCTVAR(2,:));
+    PLS_corr_perm_PAL(n,1)=corr(XS(:,1), PAL(PAL>-1 ));
+    PLS_corr_perm_PAL(n,2)=corr(XS(:,2), PAL(PAL>-1 ));
+    PLS_corr_perm_PAL(n,3)=corr(XS(:,3), PAL(PAL>-1 ));
+    PLS_corr_perm_PAL(n,4)=corr(XS(:,4), PAL(PAL>-1 ));
+    PLS_corr_perm_AGE(n,1)=corr(XS(:,4), age_short(PAL>-1));
+    PLS_corr_perm_Tstat(n,1)=corr(XL(:,1), tstat_roi1');
+    PLS_corr_perm_Tstat(n,2)=corr(XL(:,2), tstat_roi1');
+    PLS_corr_perm_Tstat(n,3)=corr(XL(:,3), tstat_roi1');
+    PLS_corr_perm_Tstat(n,4)=corr(XL(:,4), tstat_roi1');
+    PLS_groupdiff_loadings(n,1)=mean(XS(1:115,2))-mean(XS(116:230,2));
+    PLS_groupdiff_loadings(n,2)=mean(XS(1:115,4))-mean(XS(116:230,4));
+    PLS_xl_corr(n,1)=corr(XL(:,2),XL(:,4));
+    PLS_xl_corr(n,2)=corr(XL(:,2),XL(:,5));
+    PLS_xl_corr(n,3)=corr(XL(:,4),XL(:,5));
+end
+[XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(X,Y,ncomp);
+
+sum(PLS_corr_perm_PAL>corr(XS(:,4), PAL(PAL>-1)))/permutations
+corr(XS,  PAL(PAL>-1))
+sum(PLS_corr_perm_BIS>corr(XS(:,4), BIS11(PAL>-1)))/permutations
+corr(XS,  BIS11(PAL>-1))
+sum(PLS_corr_perm_AGE<corr(XS(:,4), age_short(PAL>-1)))/permutations
+corr(XS,  age_short(PAL>-1 ))
+
+PCTVAR
+
+mean(XL(consistent4,2))
+mean(XL(inconsistent4,2))
+
+[h,p, ci, stats]=ttest2((XL(consistent4|consistent1,2)), (XL(inconsistent4|inconsistent1,2)))
+
+mean(XL(consistent1,4))
+mean(XL(inconsistent1,4))
+
+%XL_zscore(:,1)=zscore(XL(:,1));XL_zscore(:,2)=zscore(XL(:,2));XL_zscore(:,3)=zscore(XL(:,3));XL_zscore(:,4)=zscore(XL(:,4));
+corr(XL, tstat_roi1')
+
+sum(PLS_corr_perm_Tstat(:,1) < corr(XL(:,1), tstat_roi1'))/permutations 
+sum(PLS_corr_perm_Tstat(:,2) > corr(XL(:,2), tstat_roi1'))/permutations 
+sum(PLS_corr_perm_Tstat(:,3) < corr(XL(:,3), tstat_roi1'))/permutations 
+sum(PLS_corr_perm_Tstat(:,4) > corr(XL(:,4), tstat_roi1'))/permutations 
+
+
+
+corr(Y)
+
+sum(PLS_xl_corr(:,1) < corr(XL(:,2), XL(:,4)))/permutations 
+sum(PLS_xl_corr(:,2) < corr(XL(:,2), XL(:,5)))/permutations 
+sum(PLS_xl_corr(:,3) < corr(XL(:,4), XL(:,5)))/permutations 
+
+
+%XS aka component loadings for the non age related component are higher for
+%the stimulant group than the HC group
+% PLS1 aka ageing component
+[h,p, ci, stats]=ttest2(XS(1:115,2),XS(116:230,2)) % t=-1.6846
+sum(PLS_groupdiff_loadings(:,1) < mean(XS(1:115,2))-mean(XS(116:230,2)))/permutations %p=0.1492
+
+% PLS2 aka the group component
+[h,p, ci, stats]=ttest2(XS(1:115,4),XS(116:230,4)) % t=-5.4071
+sum(PLS_groupdiff_loadings(:,1) < mean(XS(1:115,4))-mean(XS(116:230,4)))/permutations %p<0.001
+
+g=group(group<=2);g=g(PAL>-1); XS(g==2)
+
+
+%% PLOTTING!!!! check that the PLS component 2 predicts PAL
+    %%%%%%%%%% PLS2 vs age and PLS2 vs 
+    RGB_back=[192 192 192]/256;
+figure(3); subplot(2,2,1);
+    scatter(XS(:,4), PAL(PAL>-1), 20,RGB_back,'filled'); x=XS(:,4); y=PAL(PAL>-1); corr(x,y)
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k','LineWidth',1); %ylabel('GSK'); xlabel('ATX');
+subplot(2,2,3);
+ scatter(XS(:,4), age_short(PAL>-1 ), 20,RGB_back, 'filled'); x=XS(:,4); y=age_short(PAL>-1  ); corr(x,y)
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k','LineWidth',1);ylim([20 60]); %ylabel('GSK'); xlabel('ATX');
+
+subplot(2,2,2);
+    scatter(XS(:,2), PAL(PAL>-1), 20,RGB_back, 'filled'); x=XS(:,2); y=PAL(PAL>-1); corr(x,y)
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k','LineWidth',1); %ylabel('GSK'); xlabel('ATX');
+subplot(2,2,4);
+    scatter(XS(:,2), age_short(PAL>-1), 20,RGB_back, 'filled'); x=XS(:,2); y=age_short(PAL>-1); corr(x,y)
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k','LineWidth',1); ylim([20 60]);%ylabel('GSK'); xlabel('ATX');
+RGB_HC=[36 151 195]/256; RGB_SDI=[228 162 46]/256;
+    hold on; figure(3); subplot(2,2,1);
+    scatter(XS(1:115,4), PAL(PAL>-1 & group(group<3)==1), 15, RGB_HC, 'filled'); x=XS(:,4); y=PAL(PAL>-1); corr(x,y)
+    hold on; figure(3); subplot(2,2,1);
+    scatter(XS(116:230,4), PAL(PAL>-1 & group(group<3)==2), 15, RGB_SDI, 'filled'); x=XS(:,4); y=PAL(PAL>-1); corr(x,y)
+    hold on; figure(3); subplot(2,2,3);
+    scatter(XS(1:115,4), age_short(PAL>-1 & group(group<3)==1), 15, RGB_HC, 'filled'); x=XS(:,4); y=PAL(PAL>-1); corr(x,y)
+    hold on; figure(3); subplot(2,2,3);
+    scatter(XS(116:230,4), age_short(PAL>-1 & group(group<3)==2), 15, RGB_SDI, 'filled'); x=XS(:,4); y=PAL(PAL>-1); corr(x,y)
+    
+    hold on; figure(3); subplot(2,2,2);
+    scatter(XS(1:115,2), PAL(PAL>-1 & group(group<3)==1), 15, RGB_HC, 'filled'); x=XS(:,4); y=PAL(PAL>-1); corr(x,y)
+    hold on; figure(3); subplot(2,2,2);
+    scatter(XS(116:230,2), PAL(PAL>-1 & group(group<3)==2), 15, RGB_SDI, 'filled'); x=XS(:,4); y=PAL(PAL>-1); corr(x,y)
+    hold on; figure(3); subplot(2,2,4);
+    scatter(XS(1:115,2), age_short(PAL>-1 & group(group<3)==1), 15, RGB_HC, 'filled'); x=XS(:,4); y=PAL(PAL>-1); corr(x,y)
+    hold on; figure(3); subplot(2,2,4);
+    scatter(XS(116:230,2), age_short(PAL>-1 & group(group<3)==2), 15, RGB_SDI, 'filled'); x=XS(:,4); y=PAL(PAL>-1); corr(x,y)
+   %scatter(xX, yX, 5, RGB, 'filled')
+    
+        %%% zscore for component 1 correlates with tstat_roi1 but it's not
+        %%% significant after permutations and also this component does not
+        %%% explain a sign amount of variance
+figure; scatter(XL(:,1), tstat_roi1, 25,'filled'); x=XL(:,1); y=tstat_roi1'; corr(x,y)
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); %ylabel('GSK'); xlabel('ATX');
+
+%%%%%%visualising the volumetric PLS loadings - making the custom color
+%%%%%%maps: negative
+    R=[0:0.143:36]/256
+    G=[0:0.6:151]/256
+    B=[0:0.775:195]/256
+    RGB=horzcat(R',G',B')
+    
+    %%%%%%maps: positive
+    B=[0:0.186:46]/256
+    G=[0:0.655:162]/256
+    R=[0:0.92:228]/256
+    RGB=horzcat(R',G',B')
+    %https://users.fmrib.ox.ac.uk/~paulmc/fsleyes/userdoc/latest/customising.html#customising-rgb-colour-map-files
+%% Elementwise multiplication and case-likeness
+
+for i=1:183
+    case_likeness(i)=sum(MS(i+148,:).*tstat_roi1); 
+end
+
+corr(case_likeness', PAL(group==2), 'rows','complete')
+
