91192f87f9ba79bdeee90b8e1956180044c6c890
Peter Zhukovsky <38474927+peterzhukovsky@users.noreply.github.com>
2021-03-17 09:32:39 -0400

    Add files via upload

--- network_viz_fs.m
+++ network_viz_fs.m
@@ -2,9 +2,9 @@
 llabel_names={'L_10d_ROI.label';'L_10pp_ROI.label';'L_10r_ROI.label';'L_10v_ROI.label';'L_11l_ROI.label';'L_13l_ROI.label';'L_1_ROI.label';'L_23c_ROI.label';'L_23d_ROI.label';'L_24dd_ROI.label';'L_24dv_ROI.label';'L_25_ROI.label';'L_2_ROI.label';'L_31a_ROI.label';'L_31pd_ROI.label';'L_31pv_ROI.label';'L_33pr_ROI.label';'L_3a_ROI.label';'L_3b_ROI.label';'L_43_ROI.label';'L_44_ROI.label';'L_45_ROI.label';'L_46_ROI.label';'L_47l_ROI.label';'L_47m_ROI.label';'L_47s_ROI.label';'L_4_ROI.label';'L_52_ROI.label';'L_55b_ROI.label';'L_5L_ROI.label';'L_5m_ROI.label';'L_5mv_ROI.label';'L_6a_ROI.label';'L_6d_ROI.label';'L_6ma_ROI.label';'L_6mp_ROI.label';'L_6r_ROI.label';'L_6v_ROI.label';'L_7AL_ROI.label';'L_7Am_ROI.label';'L_7m_ROI.label';'L_7PC_ROI.label';'L_7PL_ROI.label';'L_7Pm_ROI.label';'L_8Ad_ROI.label';'L_8Av_ROI.label';'L_8BL_ROI.label';'L_8BM_ROI.label';'L_8C_ROI.label';'L_9-46d_ROI.label';'L_9a_ROI.label';'L_9m_ROI.label';'L_9p_ROI.label';'L_a10p_ROI.label';'L_A1_ROI.label';'L_a24pr_ROI.label';'L_a24_ROI.label';'L_a32pr_ROI.label';'L_a47r_ROI.label';'L_A4_ROI.label';'L_A5_ROI.label';'L_a9-46v_ROI.label';'L_AAIC_ROI.label';'L_AIP_ROI.label';'L_AVI_ROI.label';'L_d23ab_ROI.label';'L_d32_ROI.label';'L_DVT_ROI.label';'L_EC_ROI.label';'L_FEF_ROI.label';'L_FFC_ROI.label';'L_FOP1_ROI.label';'L_FOP2_ROI.label';'L_FOP3_ROI.label';'L_FOP4_ROI.label';'L_FOP5_ROI.label';'L_FST_ROI.label';'L_H_ROI.label';'L_i6-8_ROI.label';'L_IFJa_ROI.label';'L_IFJp_ROI.label';'L_IFSa_ROI.label';'L_IFSp_ROI.label';'L_Ig_ROI.label';'L_IP0_ROI.label';'L_IP1_ROI.label';'L_IP2_ROI.label';'L_IPS1_ROI.label';'L_LBelt_ROI.label';'L_LIPd_ROI.label';'L_LIPv_ROI.label';'L_LO1_ROI.label';'L_LO2_ROI.label';'L_LO3_ROI.label';'L_MBelt_ROI.label';'L_MIP_ROI.label';'L_MI_ROI.label';'L_MST_ROI.label';'L_MT_ROI.label';'L_OFC_ROI.label';'L_OP1_ROI.label';'L_OP2-3_ROI.label';'L_OP4_ROI.label';'L_p10p_ROI.label';'L_p24pr_ROI.label';'L_p24_ROI.label';'L_p32pr_ROI.label';'L_p32_ROI.label';'L_p47r_ROI.label';'L_p9-46v_ROI.label';'L_PBelt_ROI.label';'L_PCV_ROI.label';'L_PeEc_ROI.label';'L_PEF_ROI.label';'L_PFcm_ROI.label';'L_PFm_ROI.label';'L_PFop_ROI.label';'L_PF_ROI.label';'L_PFt_ROI.label';'L_PGi_ROI.label';'L_PGp_ROI.label';'L_PGs_ROI.label';'L_PHA1_ROI.label';'L_PHA2_ROI.label';'L_PHA3_ROI.label';'L_PH_ROI.label';'L_PHT_ROI.label';'L_PI_ROI.label';'L_Pir_ROI.label';'L_PIT_ROI.label';'L_pOFC_ROI.label';'L_PoI1_ROI.label';'L_PoI2_ROI.label';'L_POS1_ROI.label';'L_POS2_ROI.label';'L_PreS_ROI.label';'L_ProS_ROI.label';'L_PSL_ROI.label';'L_RI_ROI.label';'L_RSC_ROI.label';'L_s32_ROI.label';'L_s6-8_ROI.label';'L_SCEF_ROI.label';'L_SFL_ROI.label';'L_STGa_ROI.label';'L_STSda_ROI.label';'L_STSdp_ROI.label';'L_STSva_ROI.label';'L_STSvp_ROI.label';'L_STV_ROI.label';'L_TA2_ROI.label';'L_TE1a_ROI.label';'L_TE1m_ROI.label';'L_TE1p_ROI.label';'L_TE2a_ROI.label';'L_TE2p_ROI.label';'L_TF_ROI.label';'L_TGd_ROI.label';'L_TGv_ROI.label';'L_TPOJ1_ROI.label';'L_TPOJ2_ROI.label';'L_TPOJ3_ROI.label';'L_V1_ROI.label';'L_v23ab_ROI.label';'L_V2_ROI.label';'L_V3A_ROI.label';'L_V3B_ROI.label';'L_V3CD_ROI.label';'L_V3_ROI.label';'L_V4_ROI.label';'L_V4t_ROI.label';'L_V6A_ROI.label';'L_V6_ROI.label';'L_V7_ROI.label';'L_V8_ROI.label';'L_VIP_ROI.label';'L_VMV1_ROI.label';'L_VMV2_ROI.label';'L_VMV3_ROI.label';'L_VVC_ROI.label'};
 
 rlabel_names={'R_10d_ROI.label';'R_10pp_ROI.label';'R_10r_ROI.label';'R_10v_ROI.label';'R_11l_ROI.label';'R_13l_ROI.label';'R_1_ROI.label';'R_23c_ROI.label';'R_23d_ROI.label';'R_24dd_ROI.label';'R_24dv_ROI.label';'R_25_ROI.label';'R_2_ROI.label';'R_31a_ROI.label';'R_31pd_ROI.label';'R_31pv_ROI.label';'R_33pr_ROI.label';'R_3a_ROI.label';'R_3b_ROI.label';'R_43_ROI.label';'R_44_ROI.label';'R_45_ROI.label';'R_46_ROI.label';'R_47l_ROI.label';'R_47m_ROI.label';'R_47s_ROI.label';'R_4_ROI.label';'R_52_ROI.label';'R_55b_ROI.label';'R_5L_ROI.label';'R_5m_ROI.label';'R_5mv_ROI.label';'R_6a_ROI.label';'R_6d_ROI.label';'R_6ma_ROI.label';'R_6mp_ROI.label';'R_6r_ROI.label';'R_6v_ROI.label';'R_7AL_ROI.label';'R_7Am_ROI.label';'R_7m_ROI.label';'R_7PC_ROI.label';'R_7PL_ROI.label';'R_7Pm_ROI.label';'R_8Ad_ROI.label';'R_8Av_ROI.label';'R_8BL_ROI.label';'R_8BM_ROI.label';'R_8C_ROI.label';'R_9-46d_ROI.label';'R_9a_ROI.label';'R_9m_ROI.label';'R_9p_ROI.label';'R_a10p_ROI.label';'R_A1_ROI.label';'R_a24pr_ROI.label';'R_a24_ROI.label';'R_a32pr_ROI.label';'R_a47r_ROI.label';'R_A4_ROI.label';'R_A5_ROI.label';'R_a9-46v_ROI.label';'R_AAIC_ROI.label';'R_AIP_ROI.label';'R_AVI_ROI.label';'R_d23ab_ROI.label';'R_d32_ROI.label';'R_DVT_ROI.label';'R_EC_ROI.label';'R_FEF_ROI.label';'R_FFC_ROI.label';'R_FOP1_ROI.label';'R_FOP2_ROI.label';'R_FOP3_ROI.label';'R_FOP4_ROI.label';'R_FOP5_ROI.label';'R_FST_ROI.label';'R_H_ROI.label';'R_i6-8_ROI.label';'R_IFJa_ROI.label';'R_IFJp_ROI.label';'R_IFSa_ROI.label';'R_IFSp_ROI.label';'R_Ig_ROI.label';'R_IP0_ROI.label';'R_IP1_ROI.label';'R_IP2_ROI.label';'R_IPS1_ROI.label';'R_LBelt_ROI.label';'R_LIPd_ROI.label';'R_LIPv_ROI.label';'R_LO1_ROI.label';'R_LO2_ROI.label';'R_LO3_ROI.label';'R_MBelt_ROI.label';'R_MIP_ROI.label';'R_MI_ROI.label';'R_MST_ROI.label';'R_MT_ROI.label';'R_OFC_ROI.label';'R_OP1_ROI.label';'R_OP2-3_ROI.label';'R_OP4_ROI.label';'R_p10p_ROI.label';'R_p24pr_ROI.label';'R_p24_ROI.label';'R_p32pr_ROI.label';'R_p32_ROI.label';'R_p47r_ROI.label';'R_p9-46v_ROI.label';'R_PBelt_ROI.label';'R_PCV_ROI.label';'R_PeEc_ROI.label';'R_PEF_ROI.label';'R_PFcm_ROI.label';'R_PFm_ROI.label';'R_PFop_ROI.label';'R_PF_ROI.label';'R_PFt_ROI.label';'R_PGi_ROI.label';'R_PGp_ROI.label';'R_PGs_ROI.label';'R_PHA1_ROI.label';'R_PHA2_ROI.label';'R_PHA3_ROI.label';'R_PH_ROI.label';'R_PHT_ROI.label';'R_PI_ROI.label';'R_Pir_ROI.label';'R_PIT_ROI.label';'R_pOFC_ROI.label';'R_PoI1_ROI.label';'R_PoI2_ROI.label';'R_POS1_ROI.label';'R_POS2_ROI.label';'R_PreS_ROI.label';'R_ProS_ROI.label';'R_PSL_ROI.label';'R_RI_ROI.label';'R_RSC_ROI.label';'R_s32_ROI.label';'R_s6-8_ROI.label';'R_SCEF_ROI.label';'R_SFL_ROI.label';'R_STGa_ROI.label';'R_STSda_ROI.label';'R_STSdp_ROI.label';'R_STSva_ROI.label';'R_STSvp_ROI.label';'R_STV_ROI.label';'R_TA2_ROI.label';'R_TE1a_ROI.label';'R_TE1m_ROI.label';'R_TE1p_ROI.label';'R_TE2a_ROI.label';'R_TE2p_ROI.label';'R_TF_ROI.label';'R_TGd_ROI.label';'R_TGv_ROI.label';'R_TPOJ1_ROI.label';'R_TPOJ2_ROI.label';'R_TPOJ3_ROI.label';'R_V1_ROI.label';'R_v23ab_ROI.label';'R_V2_ROI.label';'R_V3A_ROI.label';'R_V3B_ROI.label';'R_V3CD_ROI.label';'R_V3_ROI.label';'R_V4_ROI.label';'R_V4t_ROI.label';'R_V6A_ROI.label';'R_V6_ROI.label';'R_V7_ROI.label';'R_V8_ROI.label';'R_VIP_ROI.label';'R_VMV1_ROI.label';'R_VMV2_ROI.label';'R_VMV3_ROI.label';'R_VVC_ROI.label'};
+label_names_all=vertcat(llabel_names, rlabel_names) %combine all labels
 
-%% RIGHT SIDE first
-clear full_cell
+%% 1. RIGHT SIDE first
 id=[1:180]'; 
 for roi=1:length(rlabel_names)
     %e.g. t=2; maxt=4 => then color should be half way to the max of the range:
@@ -12,7 +12,7 @@ for roi=1:length(rlabel_names)
         blue_range=0:255;
         green_range=178.5:0.3:255;
     
-    tstat_relative=1-tstat_roi(roi+180)/(max(tstat_roi)+0.001);
+    tstat_relative=1-tstat_roi(roi+180)/(max(tstat_roi)+0.1);
     color_relative=round(tstat_relative*256);
     
     R(roi,1)=255;
@@ -25,7 +25,7 @@ for roi=1:length(rlabel_names)
     elseif tstat_roi(roi+180)<0;
         red_range=0:255;
         green_range=183.5:0.28:255;
-    tstat_relative=1-tstat_roi(roi+180)/(min(tstat_roi)-0.001);
+    tstat_relative=1-tstat_roi(roi+180)/(min(tstat_roi)-0.1);
     color_relative=round(tstat_relative*256);
     
     
@@ -37,17 +37,9 @@ for roi=1:length(rlabel_names)
     end
 end
 
-
-
-full_cell=full_cell' %%% full cell can be used as a freesurfer colortable along with the labels to generate an aparc file with each of the labels having the color corresponding to the respective statistic
-
-formatSpec = '%d  %s   %d  %d   %d   %d\n';
-fprintf(formatSpec,full_cell{:})
-
-
 %for the blue scales: blue 255 always; green 85:255 red 0:255
 
-%% LEFT SIDE next
+%% 2. LEFT SIDE next
 clear full_cell
 id=[1:180]'; 
 for roi=1:length(rlabel_names)
@@ -56,7 +48,7 @@ for roi=1:length(rlabel_names)
         blue_range=0:255;
         green_range=178.5:0.3:255;
     
-    tstat_relative=1-tstat_roi(roi)/(max(tstat_roi)+0.001);
+    tstat_relative=1-tstat_roi(roi)/(max(tstat_roi)+0.1);
     color_relative=round(tstat_relative*256);
     
     R(roi,1)=255;
@@ -64,12 +56,12 @@ for roi=1:length(rlabel_names)
     B(roi,1)=blue_range(color_relative);
     A(roi,1)=0;
     
-    full_cell{roi, 1}=id(roi); full_cell{roi, 2}=llabel_names{roi};full_cell{roi, 3}=R(roi);    full_cell{roi, 4}=G(roi);    full_cell{roi, 5}=B(roi);     full_cell{roi, 6}=A(roi);
+    full_cell{roi, 1}=id(roi); full_cell{roi, 2}=rlabel_names{roi};full_cell{roi, 3}=R(roi);    full_cell{roi, 4}=G(roi);    full_cell{roi, 5}=B(roi);     full_cell{roi, 6}=A(roi);
 
     elseif tstat_roi(roi)<0;
         red_range=0:255;
         green_range=183.5:0.28:255;
-    tstat_relative=1-tstat_roi(roi)/(min(tstat_roi)-0.001);
+    tstat_relative=1-tstat_roi(roi)/(min(tstat_roi)-0.1);
     color_relative=round(tstat_relative*256);
     
     
@@ -77,36 +69,108 @@ for roi=1:length(rlabel_names)
     G(roi,1)=round(green_range(color_relative));
     B(roi,1)=255;
     A(roi,1)=0;
-    full_cell{roi, 1}=id(roi); full_cell{roi, 2}=llabel_names{roi};full_cell{roi, 3}=R(roi);    full_cell{roi, 4}=G(roi);    full_cell{roi, 5}=B(roi);     full_cell{roi, 6}=A(roi);
+    full_cell{roi, 1}=id(roi); full_cell{roi, 2}=rlabel_names{roi};full_cell{roi, 3}=R(roi);    full_cell{roi, 4}=G(roi);    full_cell{roi, 5}=B(roi);     full_cell{roi, 6}=A(roi);
     end
 end
 
+%% 3. Histogram of all MS strengths
+figure;
+histogram((reshape(MS_difference, [length(MS(group==2))*360,1])), 35,'FaceColor','r')
+hold on;
+histogram((reshape(MS_difference_HC, [148*360,1])), 35,'FaceColor','b')
 
-full_cell=full_cell'
+histogram((reshape(MS, [433*360,1])), 55)
 
 
-formatSpec = '%d  %s   %d  %d   %d   %d\n';
-fprintf(formatSpec,full_cell{:})
+%% 4. Histograms of age
+
+data=readtable('C:\Users\Peter\Documents\Admin files\Spring 2019\MSN_networks\Summary_sheets.xlsx');
+
+subplot(1,3,1); histogram(data.AGE(data.Group==1), 15); xlim([10 70])
+subplot(1,3,2); histogram(data.AGE(data.Group==2), 15); xlim([10 70])
+subplot(1,3,3); histogram(data.AGE(data.Group==5), 15); xlim([60 85])
 
 
-%% Age hist
+%% 5. Correlation plots
 
-histogram(age(group==4))
+            %>> later
+corr(horzcat(tstat_1', tstat_2', tstat_3', tstat_4'))
+figure; 
+subplot(2,3,1); x=tstat_1'; y=tstat_2'; scatter(x, y, 5, 'k', 'filled');
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); ylabel('ATX'); xlabel('ENDO'); xlim([-4 4]);ylim([-4 4]);
+subplot(2,3,2); x=tstat_1'; y=tstat_3'; scatter(x, y, 5, 'k', 'filled'); 
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); ylabel('GSK'); xlabel('ENDO'); xlim([-4 4]);ylim([-4 4])
+subplot(2,3,3); x=tstat_1'; y=tstat_4'; scatter(x, y, 5, 'k', 'filled'); 
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); ylabel('Ageing'); xlabel('ENDO'); xlim([-4 4]);ylim([-4 4])
+subplot(2,3,4); x=tstat_2'; y=tstat_3'; scatter(x, y, 5, 'k', 'filled'); 
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); ylabel('GSK'); xlabel('ATX'); xlim([-4 4]);ylim([-4 4])
+subplot(2,3,5); x=tstat_2'; y=tstat_4'; scatter(x, y, 5, 'k', 'filled'); 
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); ylabel('Ageing'); xlabel('ATX');xlim([-4 4]);ylim([-4 4])
+subplot(2,3,6); x=tstat_3'; y=tstat_4'; scatter(x, y, 5, 'k', 'filled'); 
+X = [ones(length(x),1) x]; b = X\y; yCalc = X*b; hold on; plot(x,yCalc,'k'); ylabel('Ageing'); xlabel('GSK'); xlim([-4 4]);ylim([-4 4])
 
-%% Boxplot the adjr2
 
-figure
-boxplot([adjr1',adjr2', adjr3'],'Notch','on')
+%% 6. Mean ms vs difference ms
+    %relies on having a tstat calculated before
+m1=mean(MS(1:148,:))
+scatter(m1,tstat_roi1, 10, 'filled'); xlim([-0.08 0.08]); hold on;
+scatter(m1(FDR<0.01), tstat_roi1(FDR<0.01), 30)
 
-boxplot([log(adjr1(adjr1>0 & adjr2>0 & adjr3>0))', log(adjr2(adjr1>0 & adjr2>0 & adjr3>0))', log(adjr3(adjr1>0 & adjr2>0 & adjr3>0))'],'Notch','on')
+x=m1; y=tstat_roi1;
+X = [ones(length(x),1) x'];  
+b = X\y'; yCalc = X*b; %xlim([-0.08 0.06])
+hold on; plot(x,yCalc, 'k','LineWidth',2); 
+corr(tstat_roi1', m1')
+                            %%%% run the permutation test on the correlation between regional 
+                            %%%% mean MS and regional t-statistics
+                    permutations=1000;   
+                    for n = 1:permutations
+                    % selecting either next combination, or random permutation
+                    permutation_index = randperm(length(allobservations));
+                            for roi=1:360;
+                            allobservations=MS(1:331,roi); sample1=MS(1:148, roi); sample2=MS(149:331, roi);
+                     
+                            % dividing into two samples
+                            randomSample1 = allobservations(permutation_index(1:length(sample1)));
+                            randomSample2 = allobservations(permutation_index(length(sample1)+1:length(permutation_index)));
 
-d2=adjr2'-adjr1'
-[h,p,ci,stats]=ttest(d2)
-mean(d2)/std(d2)
+                            % saving differences between the two samples
+                            [h,p,ci, stats]=ttest2(randomSample1, randomSample2);
+                            tstat_random(n, roi)=stats.tstat;
+                            end
+                    end
+                    
+                    %%%% permuted t-stat correlation with mean MS
+                    permutations=3000;
+                            for n = 1:permutations
+                                perm_cor(n)=corr(tstat_random(n,:)', mean_MS');
+                            end
+                            p_corr_perm=sum((perm_cor>corr(tstat_roi1', mean_MS'))/permutations)
+                    %%%% permuted t-stat correlation with the Rockland t-stat 
 
-d3=adjr3'-adjr1'
-mean(d3)/std(d3)
-[h,p,ci,stats]=ttest(d3)
-figure; yyaxis right
-scatter(ones(length(d2),1),d2, 'b', 'filled'); hold on
-scatter(2*ones(length(d3),1),d3, 'b', 'filled');xlim([0 3]);
+                                for roi=1:360
+                                [h,p, ci, stats]=ttest2( MS((group==4 & age<45 & age>20), roi), MS(group==2, roi));
+                                tstat_roi(roi)=stats.tstat;
+                                pstat_roi(roi)=p;
+                                end
+                            for n = 1:permutations
+                                perm_cor(n)=corr(tstat_random(n,:)', tstat_roi);
+                            end
+                            
+                            p_corr_perm=sum((perm_cor>corr(tstat_roi1', tstat_roi))/permutations)
+                    scatter(tstat_roi, tstat_roi1', 8, 'k', 'filled'); x=tstat_roi';  y=tstat_roi1;
+                                                        X = [ones(length(x),1) x'];  
+                                                        b = X\y'; yCalc = X*b; %xlim([-0.08 0.06])
+                                                        hold on; plot(x,yCalc, 'k','LineWidth',1); 
+
+                            
+    %   Plotting the tstat_roi vs mean)
+m1=mean(MS(1:148,:))
+scatter(m1,tstat_roi, 10, 'filled'); xlim([-0.08 0.08]); ylim([-10 8]); hold on;
+scatter(m1(FDR_hc_ixi<0.01), tstat_roi(FDR_hc_ixi<0.01), 30)
+
+x=m1; y=tstat_roi';
+X = [ones(length(x),1) x'];  
+b = X\y'; yCalc = X*b; %xlim([-0.08 0.06])
+hold on; plot(x,yCalc, 'k','LineWidth',2); 
+
